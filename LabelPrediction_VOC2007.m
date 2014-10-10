9f23bac794196a7605afdc23121aee927a3b1487
Wang Dequan <im@wangdequan.cn>
2014-10-10 11:15:02 +0800

    Balance Precision and Recall to obtain better F-Score

--- LabelPrediction_VOC2007.m
+++ LabelPrediction_VOC2007.m
@@ -4,10 +4,11 @@ X_trn = VOC2007_TrainX; X_tst = VOC2007_TestX;
 for k=1:21
   Y_trn = VOC2007_TrainY(:,k); 
   Y_tst = VOC2007_TestY(:,k);
-  model = train(Y_trn,sparse(double(X_trn)));
-  [X,Y,Z] = predict(Y_tst,sparse(double(X_tst)),model);
-  VOC2007_PreY(:,k) = X; AP(k) = Y(1);
-  fscore(X,Y_tst);
+  ratio = size(find(Y_trn==1),1)/size(find(Y_trn==0),1);
+  model = train(Y_trn,sparse(double(X_trn)),['-s 0 -w1 ' num2str(50*round(1/ratio))]);
+  [Y_hat,Accu,Deci] = predict(Y_tst,sparse(double(X_tst)),model);
+  VOC2007_PreY(:,k) = Y_hat; AP(k) = Accu(1);
+  fscore(Y_hat,Y_tst);
 end
 
 mean(AP)
